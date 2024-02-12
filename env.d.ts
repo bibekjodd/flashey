02b73a733a3f8b3f8b45123afc13633d11546eba
bibekjodd <bibekjodd@outlook.com>
2024-02-12 18:39:25 +0545

    migrate to nextjs

--- env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_BACKEND_URL: string;
-  readonly VITE_PUSHER_KEY: string;
-  readonly VITE_PUSHER_CLUSTER: string;
-  readonly VITE_GOOGLE_AUTH: string;
-}
--- src/components/MessageMain.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <section>Messages Component</section>
-</template>
